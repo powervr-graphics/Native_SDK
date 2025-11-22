4b797a344ea70f5ec1c27df61f0afe40c97c487e
ufidstudios-ch <admin@ufidstudios.ch>
2025-11-22 09:18:16 +0100

    Fix Findglm for Android case-sensitive builds

--- cmake/modules/Findglm.cmake
+++ cmake/modules/Findglm.cmake
@@ -8,15 +8,31 @@
 if(PVR_PREBUILT_DEPENDENCIES)
 	if(ANDROID)
 		set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
-		string(TOLOWER ${CMAKE_BUILD_TYPE} PVR_ANDROID_BUILD_TYPE)
+		string(TOLOWER "${CMAKE_BUILD_TYPE}" PVR_ANDROID_BUILD_TYPE)
 		message(STATUS "Debug findGlm CMAKE_CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_DIR}")
-		message(STATUS "Debug findGlm glob path ${CMAKE_CURRENT_LIST_DIR}/../../external/glm/build-android/.cxx/${PVR_ANDROID_BUILD_TYPE}/*/${ANDROID_ABI}/build}")
+		
 		# The new AGP structure includes a random hash, so we need to use a wildcard to find the correct path.
-		file(GLOB glm_DIR_GLOB "${CMAKE_CURRENT_LIST_DIR}/../../external/glm/build-android/.cxx/${PVR_ANDROID_BUILD_TYPE}/*/${ANDROID_ABI}/build")
+		# Try lowercase build type first
+		set(SEARCH_PATH "${CMAKE_CURRENT_LIST_DIR}/../../external/glm/build-android/.cxx/${PVR_ANDROID_BUILD_TYPE}/*/${ANDROID_ABI}/build")
+		message(STATUS "Debug findGlm glob path ${SEARCH_PATH}")
+		file(GLOB glm_DIR_GLOB "${SEARCH_PATH}")
+
+		# If not found, try original build type (case sensitive on Linux)
+		if(NOT glm_DIR_GLOB)
+			set(SEARCH_PATH "${CMAKE_CURRENT_LIST_DIR}/../../external/glm/build-android/.cxx/${CMAKE_BUILD_TYPE}/*/${ANDROID_ABI}/build")
+			message(STATUS "Debug findGlm glob path retry ${SEARCH_PATH}")
+			file(GLOB glm_DIR_GLOB "${SEARCH_PATH}")
+		endif()
+
 		# The glob will return a list, but there should only be one match.
 		message(STATUS "Debug findGlm glm_DIR_GLOB ${glm_DIR_GLOB}")
 
-		list(GET glm_DIR_GLOB 0 glm_DIR)
+		list(LENGTH glm_DIR_GLOB LEN)
+		if(LEN GREATER 0)
+			list(GET glm_DIR_GLOB 0 glm_DIR)
+		else()
+			message(WARNING "Could not find glm build directory")
+		endif()
 	endif()
 endif()
 
