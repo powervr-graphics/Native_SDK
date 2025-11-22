6e8c0faab1946b9186038317b5a2aa824cc5ca4b
ufidstudios-ch <admin@ufidstudios.ch>
2025-11-22 11:32:58 +0100

    Fix Android build path and pugixml host build

--- cmake/modules/FindPVRVk.cmake
+++ cmake/modules/FindPVRVk.cmake
@@ -11,9 +11,9 @@ if(PVR_PREBUILT_DEPENDENCIES)
 		set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 
 		# Use wildcard for build type to handle Debug/debug casing differences
-		file(GLOB PVRVk_DIR_GLOB "${CMAKE_CURRENT_LIST_DIR}/../../framework/PVRVk/build-android/.cxx/*/*/${ANDROID_ABI}/PVRVk")
-		
-		# The glob will return a list, but there should only be one match.
+        file(GLOB PVRVk_DIR_GLOB "${CMAKE_CURRENT_LIST_DIR}/../../framework/PVRVk/build-android/.cxx/${PVR_ANDROID_BUILD_TYPE}/*/${ANDROID_ABI}/PVRVk")
+
+        # The glob will return a list, but there should only be one match.
 		if(PVRVk_DIR_GLOB)
 			list(GET PVRVk_DIR_GLOB 0 PVRVk_DIR)
 		else()
