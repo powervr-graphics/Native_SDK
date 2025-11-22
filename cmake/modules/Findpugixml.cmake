5f22b107f877ac7d46e4ebf5c9c6023b8e1ac3f9
ufidstudios-ch <admin@ufidstudios.ch>
2025-11-22 11:37:11 +0100

    Fix Android build path and pugixml host build

--- cmake/modules/Findpugixml.cmake
+++ cmake/modules/Findpugixml.cmake
@@ -7,14 +7,22 @@
 
 if(PVR_PREBUILT_DEPENDENCIES)
 	if(ANDROID)
+		set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
 		string(TOLOWER ${CMAKE_BUILD_TYPE} PVR_ANDROID_BUILD_TYPE)
 		# The new AGP structure includes a random hash, so we need to use a wildcard to find the correct path.
 		file(GLOB pugixml_DIR_GLOB "${CMAKE_CURRENT_LIST_DIR}/../../external/pugixml/build-android/.cxx/${PVR_ANDROID_BUILD_TYPE}/*/${ANDROID_ABI}/build")
 		# The glob will return a list, but there should only be one match.
-		list(GET pugixml_DIR_GLOB 0 pugixml_DIR)
-	elseif(NOT TARGET pugixml)
-		# If not Android, and using prebuilt dependencies, we might not have a prebuilt pugixml for the host.
-		# Fallback to building from the bundled source.
+		if(pugixml_DIR_GLOB)
+			list(GET pugixml_DIR_GLOB 0 pugixml_DIR)
+		endif()
+	endif()
+endif()
+
+if(NOT TARGET pugixml)
+	if(pugixml_DIR)
+		find_package(pugixml REQUIRED CONFIG)
+	else()
+		# Fallback to building from the bundled source if prebuilt not found
 		if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../external/pugixml/CMakeLists.txt")
 			set(PVR_PREBUILT_DEPENDENCIES_CACHED ${PVR_PREBUILT_DEPENDENCIES})
 			set(PVR_PREBUILT_DEPENDENCIES OFF)
