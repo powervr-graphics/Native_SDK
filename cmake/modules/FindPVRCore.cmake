ea5f666ae60febdb8df0ef8a864860f238f592db
ufidstudios-ch <admin@ufidstudios.ch>
2025-11-22 09:54:28 +0100

    Fix FindPVRCore.cmake for Android case-sensitive builds

--- cmake/modules/FindPVRCore.cmake
+++ cmake/modules/FindPVRCore.cmake
@@ -18,6 +18,7 @@ if(NOT TARGET pugixml)
 	find_dependency(pugixml REQUIRED MODULE)
 endif()
 
+# Try to find prebuilt if configured
 if(PVR_PREBUILT_DEPENDENCIES)
 	if(ANDROID)
 		# Allow finding packages in the host file system
@@ -25,36 +26,38 @@ if(PVR_PREBUILT_DEPENDENCIES)
 
 		string(TOLOWER "${CMAKE_BUILD_TYPE}" PVR_ANDROID_BUILD_TYPE)
 
-		message(STATUS "FindPVRCore: CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}")
-		message(STATUS "FindPVRCore: PVR_ANDROID_BUILD_TYPE=${PVR_ANDROID_BUILD_TYPE}")
-		message(STATUS "FindPVRCore: ANDROID_ABI=${ANDROID_ABI}")
-		
-		# Use wildcard for build type to handle Debug/debug casing differences and other variants
+		# Search for the config file directly
 		# Try lowercase build type first
-		set(SEARCH_PATH "${CMAKE_CURRENT_LIST_DIR}/../../framework/PVRCore/build-android/.cxx/${PVR_ANDROID_BUILD_TYPE}/*/${ANDROID_ABI}/PVRCore")
-		message(STATUS "FindPVRCore: SEARCH_PATH_1=${SEARCH_PATH}")
-		file(GLOB PVRCore_DIR_GLOB "${SEARCH_PATH}")
+		set(SEARCH_PATH "${CMAKE_CURRENT_LIST_DIR}/../../framework/PVRCore/build-android/.cxx/${PVR_ANDROID_BUILD_TYPE}/*/${ANDROID_ABI}/PVRCore/PVRCoreConfig.cmake")
+		file(GLOB PVRCore_CONFIG_GLOB "${SEARCH_PATH}")
 		
-		# If not found, try original build type (case sensitive on Linux)
-		if(NOT PVRCore_DIR_GLOB)
-			set(SEARCH_PATH "${CMAKE_CURRENT_LIST_DIR}/../../framework/PVRCore/build-android/.cxx/${CMAKE_BUILD_TYPE}/*/${ANDROID_ABI}/PVRCore")
-			message(STATUS "FindPVRCore: SEARCH_PATH_2=${SEARCH_PATH}")
-			file(GLOB PVRCore_DIR_GLOB "${SEARCH_PATH}")
+		# If not found, try original build type
+		if(NOT PVRCore_CONFIG_GLOB)
+			set(SEARCH_PATH "${CMAKE_CURRENT_LIST_DIR}/../../framework/PVRCore/build-android/.cxx/${CMAKE_BUILD_TYPE}/*/${ANDROID_ABI}/PVRCore/PVRCoreConfig.cmake")
+			file(GLOB PVRCore_CONFIG_GLOB "${SEARCH_PATH}")
 		endif()
 		
-		message(STATUS "FindPVRCore: PVRCore_DIR_GLOB=${PVRCore_DIR_GLOB}")
-
-		# The glob will return a list, but there should only be one match.
-		list(LENGTH PVRCore_DIR_GLOB LEN)
-		if(LEN GREATER 0)
-			list(GET PVRCore_DIR_GLOB 0 PVRCore_DIR)
-			message(STATUS "FindPVRCore: Found PVRCore_DIR=${PVRCore_DIR}")
-		else()
-			message(WARNING "PVRCore: No build directory found matching ${CMAKE_CURRENT_LIST_DIR}/../../framework/PVRCore/build-android/.cxx/*/*/${ANDROID_ABI}/PVRCore")
+		if(PVRCore_CONFIG_GLOB)
+			list(GET PVRCore_CONFIG_GLOB 0 PVRCore_CONFIG_FILE)
+			get_filename_component(PVRCore_DIR ${PVRCore_CONFIG_FILE} DIRECTORY)
+			message(STATUS "PVRCore: Found prebuilt directory ${PVRCore_DIR}")
 		endif()
 	endif()
 endif()
 
 if(NOT TARGET PVRCore)
-	find_package(PVRCore REQUIRED CONFIG)
+	# Try to find the package configuration
+	find_package(PVRCore CONFIG QUIET)
+	
+	if(PVRCore_FOUND)
+		message(STATUS "PVRCore: Package configuration found.")
+	else()
+		message(STATUS "PVRCore: Prebuilt package not found. Attempting to build from source...")
+		set(PVRCore_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../framework/PVRCore")
+		if(EXISTS "${PVRCore_SOURCE_DIR}/CMakeLists.txt")
+			add_subdirectory("${PVRCore_SOURCE_DIR}" "${CMAKE_BINARY_DIR}/framework/PVRCore")
+		else()
+			message(FATAL_ERROR "PVRCore: Could not find prebuilt package AND could not find source at ${PVRCore_SOURCE_DIR}")
+		endif()
+	endif()
 endif()
